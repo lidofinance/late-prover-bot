95be9c997900b0fba59270b61d6ad851d48552e4
hweawer <alexandergusakovsky@gmail.com>
2025-11-28 17:08:03 +0100

    Remove custorm transport

--- src/common/logger/logger.module.ts
+++ src/common/logger/logger.module.ts
@@ -1,4 +1,4 @@
-import { LoggerModule as Logger, cleanSecrets } from '@lido-nestjs/logger';
+import { LoggerModule as Logger, simpleTransport, jsonTransport } from '@lido-nestjs/logger';
 import { Module } from '@nestjs/common';
 import * as winston from 'winston';
 
@@ -17,27 +17,8 @@ import { LogFormat } from '../config/interfaces';
         const format = configService.get('LOG_FORMAT');
         const isJSON = format === LogFormat.JSON;
 
-        const transport = new winston.transports.Console({
-          format: isJSON
-            ? winston.format.combine(
-                cleanSecrets({ secrets }),
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                winston.format.json(),
-              )
-            : winston.format.combine(
-                winston.format.colorize({ all: true }),
-                cleanSecrets({ secrets }),
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                winston.format.printf(({ timestamp, level, message, context, stack, ...meta }) => {
-                  const contextStr = context ? ` [${context}]` : '';
-                  const metaStr = Object.keys(meta).length > 0 ? ` ${JSON.stringify(meta)}` : '';
-                  const stackStr = stack ? `\n${stack}` : '';
-                  return `${timestamp} ${level}${contextStr}: ${message}${metaStr}${stackStr}`;
-                }),
-              ),
-        });
-
-        return { level, transports: [transport] };
+        const transports = isJSON ? jsonTransport({ secrets }) : simpleTransport({ secrets });
+        return { level, transports: [transports] };
       },
     }),
   ],
